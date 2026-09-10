5f8dcf92827f9cec19be103fcbd8cdfd4c2bff24
P.Laxminarsimha <Plaxminarsimha26@gmail.com>
2026-09-10 18:10:28 +0530

    Complete Code

--- Backend/controllers/authController.js
+++ Backend/controllers/authController.js
@@ -7,6 +7,7 @@ exports.signup = (req, res) => {
 
     userModel.findUserByEmail(email, (err, result) => {
         if (err) {
+            
             return res.status(500).json(err);
         }
 
