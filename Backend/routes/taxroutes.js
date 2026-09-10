b7533bca9add9c91e4f7f497f197cc528f00627c
Simha26 <plaxminarsimha26@gmail.com>
2026-09-10 17:51:54 +0530

    Complete Code

--- Backend/routes/taxroutes.js
+++ Backend/routes/taxroutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const router = express.Router();
 
-const taxController = require("../controllers/taxcontroller");
+const taxController = require("../controllers/taxController");
 
 router.get("/", taxController.getTaxEstimates);
 
